d750d7f1bf6707134020f0efd697517bf4c6253e
Himanshu Singh <singh.himanshu2408@gmail.com>
2017-11-23 15:04:05 +0530

    basic structure and implemented routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './home/login/login.component';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { PlaygroundComponent } from './home/playground/playground.component';
+import { NotFoundComponent } from './home/not-found/not-found.component';
+
+const approutes = [
+  {path: '', component: HomeComponent, children: [
+    { path: '', component: LoginComponent },
+    { path: 'play', component: PlaygroundComponent }
+  ]},
+  {path: '**', component: NotFoundComponent}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    PlaygroundComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(approutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
